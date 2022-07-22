a0016460a29662779bea515df1e749ba94722496
ApxNord <nord-life17@yandex.ru>
2022-07-22 21:12:10 +0300

    Добавление поиска по имени и жанру

--- MyCoreApp/Models/Movie.cs
+++ MyCoreApp/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyCoreApp.Models
 {
@@ -7,9 +8,12 @@ namespace MyCoreApp.Models
         public int ID { get; set; }
         public string Title { get; set; } = string.Empty;
 
+        [Display(Name="Дата выпуска")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; } = string.Empty;
+       
+        [Column(TypeName ="decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
